cc69ed0a2bd31657384bf2135779639becb0d37b
李江 <1280291927@qq.com>
2022-08-08 11:58:40 +0800

    解决打包遇到的问题

--- vue.config.js
+++ vue.config.js
@@ -7,14 +7,14 @@ module.exports = {
       // 为生产环境修改配置
     } else {
       // 为开发环境修改配置
-      return {
-        resolve: {
-          alias: {
-            '@js': path.resolve(__dirname, './src/assets/js'),
-            '@css': path.resolve(__dirname, './src/assets/css'),
-            '@img': path.resolve(__dirname, './src/assets/image'),
-            '@c': path.resolve(__dirname, './src/components')
-          }
+    }
+    return {
+      resolve: {
+        alias: {
+          '@js': path.resolve(__dirname, './src/assets/js'),
+          '@css': path.resolve(__dirname, './src/assets/css'),
+          '@img': path.resolve(__dirname, './src/assets/image'),
+          '@c': path.resolve(__dirname, './src/components')
         }
       }
     }
